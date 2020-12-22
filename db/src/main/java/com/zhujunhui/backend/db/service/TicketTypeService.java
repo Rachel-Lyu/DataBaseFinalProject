c63b61de6a70f22b7ee53bf81108cab8535bc24b
Indigo <j.c.f.gauss@sjtu.edu.cn>
2020-12-22 12:04:19 +0800

    Add 'All' condition for ticket type

--- db/src/main/java/com/zhujunhui/backend/db/service/TicketTypeService.java
+++ db/src/main/java/com/zhujunhui/backend/db/service/TicketTypeService.java
@@ -18,11 +18,17 @@ public class TicketTypeService {
 
     public List<TicketTypeDao> getAllTypes() {
         List<TicketType> types = ticketTypeRepository.findAll();
-        if(types==null)
+
+        if (types.isEmpty())
             return null;
+
+        TicketType typeAll = new TicketType();
+        typeAll.setTypeName("全部");
+        types.add(0, typeAll);
+
         List<TicketTypeDao> list = new ArrayList<>();
-        for(int i=0;i< types.size();++i){
-            list.add(new TicketTypeDao(types.get(i).getType_name()));
+        for (TicketType type : types) {
+            list.add(new TicketTypeDao(type.getTypeName()));
         }
         return list;
     }
